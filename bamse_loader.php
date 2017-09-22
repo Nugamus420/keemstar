b389927394a138eb9817d81d635537c1fc8a64b0
Nugamus420 <nmurphy5@mail.pima.edu>
2017-09-22 11:01:22 +0200

    Add files via upload

---
+++ bamse_loader.php
@@ -0,0 +1,104 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Velkommen til Bamses helt egen hjemmeside - dr.dk/oline</title>
+<script language="javascript" src="http://www.dr.dk/DRWebStat/DRflash.drxml?version=20061130"></script>
+<script language="javascript" type="text/javascript">AC_FL_RunContent = 0;</script>
+<script src="../_global/AC_RunActiveContent.js" language="javascript" type="text/javascript"></script>
+
+</head>
+<body bgcolor="#FFFFFF" marginwidth="0" marginheight="0" topmargin="0" leftmargin="0">
+<div align="center" style="padding: 15px;">
+<!--#config timefmt="%m" -->
+<script language="javascript" type="text/javascript">
+	
+	var q=window.location.search;
+	if(q){
+		var q_string=q.split("&");
+		var q_value=new Array();
+		for(var n=0;q_string[n]!=undefined;n++){
+			q_value.push(q_string[n]);
+		}
+		var myGoal=q_value[1].split("=")[1];
+		//alert(myGoal);
+	}
+
+
+if(myGoal=="bamlet"){
+	if (AC_FL_RunContent == 0) {
+		alert("This page requires AC_RunActiveContent.js. In Flash, run \"Apply Active Content Update\" in the Commands menu to copy AC_RunActiveContent.js to the HTML output folder.");
+	} else {
+		AC_FL_RunContent(
+			'codebase', 'http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0',
+			'width', '95%',
+			'height', '95%',
+			'src', 'bamlet_loader',
+			'quality', 'high',
+			'pluginspage', 'http://www.macromedia.com/go/getflashplayer',
+			'align', 'middle',
+			'play', 'true',
+			'loop', 'true',
+			'scale', 'showall',
+			'wmode', 'window',
+			'devicefont', 'false',
+			'id', 'loader',
+			'bgcolor', '#FFFFFF',
+			'name', 'bamlet_loader',
+			'menu', 'false',
+			'allowScriptAccess','sameDomain',
+			//'FlashVars','_home='+q_value[0].split("=")[1]+'&_goal='+q_value[1].split("=")[1]+'&_date=09',
+			'movie', 'bamlet_loader',
+			'salign', ''
+			); //end AC code
+	}
+}else{
+
+	if (AC_FL_RunContent == 0) {
+		alert("This page requires AC_RunActiveContent.js. In Flash, run \"Apply Active Content Update\" in the Commands menu to copy AC_RunActiveContent.js to the HTML output folder.");
+	} else {
+		AC_FL_RunContent(
+			'codebase', 'http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0',
+			'width', '95%',
+			'height', '95%',
+			'src', 'bamses_loader',
+			'quality', 'high',
+			'pluginspage', 'http://www.macromedia.com/go/getflashplayer',
+			'align', 'middle',
+			'play', 'true',
+			'loop', 'true',
+			'scale', 'showall',
+			'wmode', 'window',
+			'devicefont', 'false',
+			'id', 'loader',
+			'bgcolor', '#FFFFFF',
+			'name', 'bamses_loader',
+			'menu', 'false',
+			'allowScriptAccess','sameDomain',
+			'FlashVars','_home='+q_value[0].split("=")[1]+'&_goal='+q_value[1].split("=")[1]+'&_date=09',
+			'movie', 'bamses_loader',
+			'salign', ''
+			); //end AC code
+	}
+	}
+	
+	
+</script>
+
+</div>
+
+
+
+<!--For Oline: zZCbG6LQpKyqZlO1F4uKSPV4zSYFEkMDDUY_2lapgUP.47 -->
+<script type="text/javascript">
+if (typeof(pp_gemius_identifier)=='undefined' ) var pp_gemius_identifier = new String('zZCbG6LQpKyqZlO1F4uKSPV4zSYFEkMDDUY_2lapgUP.47');
+var g_furl =  new String('www.dr.dk/oline/bamse');
+if (typeof(g_furl)=='undefined' ) var  g_furl =  new String('www.dr.dk/oline/');
+</script>
+<script type="text/javascript" src="http://www.dr.dk/drWebStat/drWebStat.js"></script>
+<script type="text/javascript">
+getTMqs('http', g_furl, '', 'dr_dk', 'dk', 'ISO-8859-15');
+</script>
+<!-- DR STATS -->
+<script language="Javascript" src="http://www.dr.dk/DRWebStat/DRflash_bottom.drxml?version=20061130"></script></body>
+</html>
